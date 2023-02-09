2b76ea680270c3b397ced0a86b53f22435f853d8
Hyeonil Choi <tt2t2am1118@naver.com>
2023-02-10 00:23:25 +0900

    //How make code, check this. Reference, https://blog.naver.com/tt2t2am1118/222967998751 //Hope good developing~. Have a good day~. Thank you.

--- src/main/java/com/example/tddspringex01/TddClass.java
+++ src/main/java/com/example/tddspringex01/TddClass.java
@@ -6,5 +6,7 @@ public class TddClass {
 
     public static void main(String[] args) {
        User user01 = new User();
+       //How make code, check this. Reference, https://blog.naver.com/tt2t2am1118/222967998751
+        //Hope good developing~. Have a good day~. Thank you.
     }
 }
