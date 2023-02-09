8ba2c3900f49af8d8efcbf18c409c4e926280bcc
Hyeonil Choi <tt2t2am1118@naver.com>
2023-02-10 00:28:38 +0900

    Good developing~.  Next how to yours~. Thank you.

--- src/main/java/com/example/tddspringex01/TddClass.java
+++ src/main/java/com/example/tddspringex01/TddClass.java
@@ -5,8 +5,18 @@ import com.example.domain.User;
 public class TddClass {
 
     public static void main(String[] args) {
-       User user01 = new User();
+
        //How make code, check this. Reference, https://blog.naver.com/tt2t2am1118/222967998751
         //Hope good developing~. Have a good day~. Thank you.
+        //Writing ...
+        //Example,
+        /**
+         * Product01 a = new Product01("Sweater01", "Cloths", false, null, "Image", "ProductDescription...", "10000", "2500");
+         *
+         * User01 u = new User01("abcId","abcPw","abcName","abcEmail","abcPhone","abcAddress","abcAddressDetail","abcAddressPostCode");
+         *
+         *
+         * */
+
     }
 }
